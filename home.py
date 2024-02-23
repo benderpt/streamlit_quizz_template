4c35f66ce5a60cd30b416ef5b26f88e1b250fd44
Hugo Filipe Matos Almeida Moreira <122613212+benderpt@users.noreply.github.com>
2024-02-23 17:27:25 +0000

    version 0.1

--- home.py
+++ home.py
@@ -1,50 +1,113 @@
 import streamlit as st
 from streamlit.logger import get_logger
+import json
 
+# Set page configuration
 LOGGER = get_logger(__name__)
-
 def run():
     st.set_page_config(
-        page_title="Setor Público Português",
-        page_icon="🏛️",
+        page_title="Streamlit quizz app",
+        page_icon="❓",
     )
 
 if __name__ == "__main__":
     run()
 
-#sidebar
-    
-with open('styles.css') as f:
-    st.sidebar.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+# Custom CSS for the buttons
+st.markdown("""
+<style>
+div.stButton > button:first-child {
+    display: block;
+    margin: 0 auto;
+}
+</style>
+""", unsafe_allow_html=True)
 
-st.sidebar.markdown(""" 
-                    ### info
-                    Esta aplicação é baseada na informação pública do sistema de informação da organização do estado. A base de dados pode ser encontrada em [sioe.dgaep.gov.pt](https://www.sioe.dgaep.gov.pt/)""", unsafe_allow_html=True)    
+# Load quiz data
+with open('content/quiz_data.json', 'r', encoding='utf-8') as f:
+    quiz_data = json.load(f)
 
-# Body
-st.markdown ("""
-             # Setor Público Português 🏛️ 
-             ___ """)
+# Initialize session variables if they do not exist
+if 'current_index' not in st.session_state:
+    st.session_state['current_index'] = 0
+if 'current_question' not in st.session_state:
+    st.session_state['current_question'] = 0
+if 'score' not in st.session_state:
+    st.session_state.score = 0
+if 'selected_option' not in st.session_state:
+    st.session_state.selected_option = None
+if 'answer_submitted' not in st.session_state:
+    st.session_state.answer_submitted = False
 
-st.markdown(
-    """
-    Nesta aplicação é possível visualizar a informação relativa ao universo organizacional do setor público português. O objetivo é disponibilizar informação de forma acessível e intuitiva, permitindo a exploração de dados de forma interativa. Na interação com a aplicação pretende-se que o utilizador fique a conhecer a estrutura organizacional do setor público.
+def restart_quiz():
+    st.session_state.current_index = 0
+    st.session_state.score = 0
+    st.session_state.selected_option = None
+    st.session_state.answer_submitted = False
 
-    ___ """
-)
+def submit_answer():
+    # Check if an option has been selected
+    if st.session_state.selected_option is not None:
+        # Mark the answer as submitted
+        st.session_state.answer_submitted = True
+        # Check if the selected option is correct
+        if st.session_state.selected_option == quiz_data[st.session_state.current_index]['answer']:
+            st.session_state.score += 10
+    else:
+        # If no option selected, show a message and do not mark as submitted
+        st.warning("Please select an option before submitting.")
 
+def next_question():
+    st.session_state.current_index += 1
+    st.session_state.selected_option = None
+    st.session_state.answer_submitted = False
 
-# Define a pergunta e a resposta
-pergunta = "Quantos organismos tem o setor público no universo das contas nacionais?"
-resposta = "8500"
+# Title and description
+st.title("Streamlit Quiz App")
 
-# Mostra a pergunta numa "carta"
-st.subheader(pergunta)  # Ou st.write(pergunta) para um texto normal
+# Progress bar
+progress_bar_value = (st.session_state.current_index + 1) / len(quiz_data)
+st.metric(label="Score", value=f"{st.session_state.score} / {len(quiz_data) * 10}")
+st.progress(progress_bar_value)
 
-# Cria um botão que, quando clicado, mostrará a resposta
-if st.button('Ver resposta'):
-    # Mostra a resposta abaixo do botão
-    st.subheader(resposta)  # Ou st.write(resposta) para um texto normal
+# Display the question and answer options
+question_item = quiz_data[st.session_state.current_index]
+st.subheader(f"Question {st.session_state.current_index + 1}")
+st.title(f"{question_item['question']}")
+st.write(question_item['information'])
 
+st.markdown(""" ___""")
+
+# Answer selection
+options = question_item['options']
+correct_answer = question_item['answer']
+
+if st.session_state.answer_submitted:
+    for i, option in enumerate(options):
+        label = option
+        if option == correct_answer:
+            st.success(f"{label} (Correct answer)")
+        elif option == st.session_state.selected_option:
+            st.error(f"{label} (Incorrect answer)")
+        else:
+            st.write(label)
+else:
+    for i, option in enumerate(options):
+        if st.button(option, key=i, use_container_width=True):
+            st.session_state.selected_option = option
+
+st.markdown(""" ___""")
+
+# Submission button and response logic
+if st.session_state.answer_submitted:
+    if st.session_state.current_index < len(quiz_data) - 1:
+        st.button('Next', on_click=next_question)
+    else:
+        st.write(f"Quiz completed! Your score is: {st.session_state.score} / {len(quiz_data) * 10}")
+        if st.button('Restart', on_click=restart_quiz):
+            pass
+else:
+    if st.session_state.current_index < len(quiz_data):
+        st.button('Submit', on_click=submit_answer)
 
 
